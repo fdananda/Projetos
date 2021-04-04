63d1a745575ab98afe996d0c8b6a3ed4e3035f1b
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-04-04 07:43:11 -0300

    Create MainActivity.java

---
+++ minhastarefas/java/MainActivity.java
@@ -0,0 +1,72 @@
+package com.fdananda.minhastarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DiariasFragment diariasFragment;
+    private EventuaisFragment eventuaisFragment;
+    private AdicionarFragment adicionarFragment;
+    private Button buttonAtvsDiarias;
+    private Button buttonAtvsEventuais;
+    private Button buttonAtvsAdicionar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Inicializar componentes
+        diariasFragment   = new DiariasFragment();
+        eventuaisFragment = new EventuaisFragment();
+        adicionarFragment = new AdicionarFragment();
+        buttonAtvsDiarias = findViewById(R.id.buttonAtvsDiarias);
+        buttonAtvsEventuais = findViewById(R.id.buttonAtvsEventuais);
+        buttonAtvsAdicionar = findViewById(R.id.buttonAtvsAdicionar);
+
+        //Apresentar o conteúdo inicial (DiariasFragment) sem o clique do botão
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameConteudo, diariasFragment);
+        transaction.commit();
+
+        //Clique no botão Diárias
+        buttonAtvsDiarias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Apresentar o conteúdo DiáiasFragment com o clique do botão
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, diariasFragment);
+                transaction.commit();
+            }
+        });
+
+        //Clique no botão Eventuais
+        buttonAtvsEventuais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Apresentar o conteúdo DiáiasFragment com o clique do botão
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, eventuaisFragment);
+                transaction.commit();
+            }
+        });
+
+        //Clique no botão Adicionar
+        buttonAtvsAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Apresentar o conteúdo DiáiasFragment com o clique do botão
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, adicionarFragment);
+                transaction.commit();
+            }
+        });
+    }
+}
