7909fad874d21d4da86c56c9554f926d5c54587c
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2021-04-06 21:36:40 -0300

    Update MainActivity.java

--- minhastarefas/java/MainActivity.java
+++ minhastarefas/java/MainActivity.java
@@ -1,13 +1,17 @@
 package com.fdananda.minhastarefas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,17 +20,20 @@ public class MainActivity extends AppCompatActivity {
     private AdicionarFragment adicionarFragment;
     private VerTodasFragment verTodasFragment;
     private RealizadasFragment realizadasFragment;
+    private ARealizarFragment aRealizarFragment;
     private Button buttonAtvsDiarias;
     private Button buttonAtvsEventuais;
     private Button buttonAtvsAdicionar;
-    private TextView textVerTodas;
-    private TextView textRealizadas;
-    private TextView textAReaizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        toolbar.setTitle("Minhas Tarefas");
+        setSupportActionBar(toolbar);
 
         //Inicializar componentes
         diariasFragment   = new DiariasFragment();
@@ -34,12 +41,11 @@ public class MainActivity extends AppCompatActivity {
         adicionarFragment = new AdicionarFragment();
         verTodasFragment  = new VerTodasFragment();
         realizadasFragment = new RealizadasFragment();
+        aRealizarFragment = new ARealizarFragment();
         buttonAtvsDiarias = findViewById(R.id.buttonAtvsDiarias);
         buttonAtvsEventuais = findViewById(R.id.buttonAtvsEventuais);
         buttonAtvsAdicionar = findViewById(R.id.buttonAtvsAdicionar);
-        textVerTodas        = findViewById(R.id.textVerTodas);
-        textRealizadas      = findViewById(R.id.textRealizadas);
-        textAReaizar        = findViewById(R.id.textARealizar);
+
 
         //Apresentar o conteúdo inicial (DiariasFragment) sem o clique do botão
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -81,29 +87,40 @@ public class MainActivity extends AppCompatActivity {
                 transaction.commit();
             }
         });
+    }
 
-        //Clique no texto Ver Todas
-        textVerTodas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-                //Apresentar o conteúdo AdicionarFragment com o clique do botão
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.verTodasMenu:
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 transaction.replace(R.id.frameConteudo, verTodasFragment);
                 transaction.commit();
-            }
-        });
+                break;
 
-        //Clique no texto Ver Todas
-        textRealizadas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            case R.id.verRealizadasMenu:
+                FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+                transaction2.replace(R.id.frameConteudo, realizadasFragment);
+                transaction2.commit();
+                break;
 
-                //Apresentar o conteúdo AdicionarFragment com o clique do botão
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.frameConteudo, realizadasFragment);
-                transaction.commit();
-            }
-        });
+            case R.id.verARelizarMenu:
+                FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction();
+                transaction3.replace(R.id.frameConteudo, aRealizarFragment);
+                transaction3.commit();
+                break;
+
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
